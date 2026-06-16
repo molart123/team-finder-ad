8fcd019e8657557b84d6f3b4ebff79000fc735f0
stormy <artemolibozenko@gmail.com>
2026-06-16 21:06:40 +0300

    update file system

--- backend/team_finder/settings.py
+++ backend/team_finder/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "frontend/static"]
 # Media files
 
 MEDIA_URL = "/media/"
