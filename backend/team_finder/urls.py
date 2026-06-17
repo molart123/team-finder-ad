5e00f05483b50f6c61b56b9f4eb6e99c7e2ec2df
stormy <artemolibozenko@gmail.com>
2026-06-17 21:11:51 +0300

    Users all

--- backend/team_finder/urls.py
+++ backend/team_finder/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.projects.urls')),
-    path('users/', include('apps.users.urls')),
+    path('', include('apps.users.urls')),
 ]
 
 if settings.DEBUG:
